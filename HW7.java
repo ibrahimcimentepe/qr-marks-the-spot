f2b64c6c71a20c94eba0a2062b4b57ad2ec3956e
cemheren@gmail.com <cemheren@gmail.com@47a1c95e-a7b4-0c84-69ca-ea3910dde828>
2011-05-28 15:59:17 +0000

    addded a main part

--- HW7.java
+++ HW7.java
@@ -5,6 +5,10 @@ class HW7 {
 	    return 1;
 	else return fibonacci(n-1) + fibonacci (n - 2);
     }
-
+ public static void main(String[] args)  {
+  
+    fib1 = fibonacci(32);
+    System.out.println(fib1);
+ } 
 
 }
