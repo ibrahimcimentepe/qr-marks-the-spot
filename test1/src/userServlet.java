fbf22f51a74058e8fb469a4705cfdc3b92ab751a
cemheren <cemheren@47a1c95e-a7b4-0c84-69ca-ea3910dde828>
2011-10-31 14:54:10 +0000

    user falan

---
+++ test1/src/userServlet.java
@@ -0,0 +1,26 @@
+import java.io.IOException;
+
+import javax.servlet.Servlet;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+public class userServlet extends HttpServlet implements Servlet {
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+		user u =  new user();
+		u.setUserName(req.getParameter("username"));
+		u.setPassword(req.getParameter("pass"));
+		
+		
+		super.doPost(req, resp);
+	}
+
+	
+}
