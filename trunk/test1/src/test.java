35a0ef4e27d84b581a739e2645fa6a3c911874ce
cemheren <cemheren@47a1c95e-a7b4-0c84-69ca-ea3910dde828>
2011-10-31 14:54:10 +0000

    user falan

--- trunk/test1/src/test.java
+++ trunk/test1/src/test.java
@@ -1,12 +1,27 @@
+import java.util.ArrayList;
+
+import com.sun.xml.internal.bind.v2.schemagen.xmlschema.List;
+
 
 public class test {
 
 	/**
 	 * @param args
 	 */
+	
+	private ArrayList<user> Users = new ArrayList<user>();
+	
+	
+	public void addUser(user user){
+		
+		Users.add(user);
+	}
+	
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		
 		
 	}
 
