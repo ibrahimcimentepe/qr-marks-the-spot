bdb5fd291c3d11366eefd0f01d3d0acbf84985af
alikerimerkan@gmail.com <alikerimerkan@gmail.com@47a1c95e-a7b4-0c84-69ca-ea3910dde828>
2011-12-14 21:51:21 +0000

    homw.java ,showqr.java and related mysql commands are submitted

--- qrmarksspot/src/java/qrmarksspot/showQr.java
+++ qrmarksspot/src/java/qrmarksspot/showQr.java
@@ -5,10 +5,13 @@
 
 package qrmarksspot;
 
+import Classes.MySqlConnection;
 import com.sun.rave.web.ui.appbase.AbstractPageBean;
 import com.sun.webui.jsf.model.SingleSelectOptionsList;
+import java.sql.ResultSet;
 import java.util.ArrayList;
 import javax.faces.FacesException;
+import javax.faces.event.ValueChangeEvent;
 
 /**
  * <p>Page bean that corresponds to a similarly named JSP page.  This
@@ -41,18 +44,75 @@ public class showQr extends AbstractPageBean {
     public void setGameStepDropDownDefaultOptions(SingleSelectOptionsList ssol) {
         this.gameStepDropDownDefaultOptions = ssol;
     }
-    private SingleSelectOptionsList gameNameDropDownDefaultOptions = new SingleSelectOptionsList();
-
-    public SingleSelectOptionsList getGameNameDropDownDefaultOptions() {
-        return gameNameDropDownDefaultOptions;
-    }
-
-    public void setGameNameDropDownDefaultOptions(SingleSelectOptionsList ssol) {
-        this.gameNameDropDownDefaultOptions = ssol;
-    }
 
     // </editor-fold>
     ArrayList<String> gameNames = new ArrayList();
+    String selectedGameName = new String();
+    String selectedGameStepQR = new String();
+    String selectedGameStepName = new String();
+
+
+    ArrayList<String> gamesteps_Numberofsteps = new ArrayList<String>();
+    ArrayList<String> gamesteps_qrString = new ArrayList<String>();
+    ArrayList<String> gameIDs = new ArrayList();
+
+
+    public String getSelectedGameStepName() {
+        return selectedGameStepName;
+    }
+
+    public void setSelectedGameStepName(String selectedGameStepName) {
+        this.selectedGameStepName = selectedGameStepName;
+    }
+    public String getSelectedGameStepQR() {
+        return selectedGameStepQR;
+    }
+
+    public void setSelectedGameStepQR(String selectedGameStepQR) {
+        this.selectedGameStepQR = selectedGameStepQR;
+    }
+    public String getSelectedGameName() {
+        return selectedGameName;
+    }
+
+    public void setSelectedGameName(String selectedGameName) {
+        this.selectedGameName = selectedGameName;
+    }
+
+
+
+    public ArrayList<String> getGamesteps_Numberofsteps() {
+        return gamesteps_Numberofsteps;
+    }
+
+    public void setGamesteps_Numberofsteps(ArrayList<String> gamesteps_Numberofsteps) {
+        this.gamesteps_Numberofsteps = gamesteps_Numberofsteps;
+    }
+
+    public ArrayList<String> getGamesteps_qrString() {
+        return gamesteps_qrString;
+    }
+
+    public void setGamesteps_qrString(ArrayList<String> gamesteps_qrString) {
+        this.gamesteps_qrString = gamesteps_qrString;
+    }
+
+
+    public ArrayList<String> getGameIDs() {
+        return gameIDs;
+    }
+
+    public void setGameIDs(ArrayList<String> gameIDs) {
+        this.gameIDs = gameIDs;
+    }
+
+    public ArrayList<String> getGameNames() {
+        return gameNames;
+    }
+
+    public void setGameNames(ArrayList<String> gameNames) {
+        this.gameNames = gameNames;
+    }
 
 
 
@@ -82,6 +142,27 @@ public class showQr extends AbstractPageBean {
         // Perform application initialization that must complete
         // *before* managed components are initialized
         // TODO - add your own initialiation code here
+
+       // for(int i = 0;i<labels.length;i++){
+
+       //     labels[i] = " ";
+       // }
+
+        int i = 0;
+        try{
+            MySqlConnection con = new MySqlConnection();
+            ResultSet rs = con.getGameNameByUserId(this.getSessionBean1().getUserId());
+            while(rs.next()){
+
+                gameNames.add(rs.getString("gameName"));
+                gameIDs.add(rs.getString("gameId"));
+
+                //SessionBean1["label"] = labels[i];
+            }
+
+        }catch(Exception e){
+
+        }
         
         // <editor-fold defaultstate="collapsed" desc="Managed Component Initialization">
         // Initialize automatically managed components
@@ -159,6 +240,37 @@ public class showQr extends AbstractPageBean {
      */
     protected ApplicationBean1 getApplicationBean1() {
         return (ApplicationBean1) getBean("ApplicationBean1");
+    }
+
+    public void gameNameDropDown_processValueChange(ValueChangeEvent event) {
+
+        gamesteps_Numberofsteps.clear();
+        gamesteps_qrString.clear();
+
+        try{
+            MySqlConnection con = new MySqlConnection();
+
+            int index = gameNames.indexOf(selectedGameName);
+
+            ResultSet rs = con.getGameStepsByGameId(gameIDs.get(index).toString());
+            while(rs.next()){
+
+                gamesteps_Numberofsteps.add(rs.getString("StepNumber"));
+                gamesteps_qrString.add(rs.getString("QrString"));
+
+                //SessionBean1["label"] = labels[i];
+            }
+
+        }catch(Exception e){
+
+        }
+    }
+
+    public void gameStepDropDown_processValueChange(ValueChangeEvent event) {
+
+        int index = gamesteps_Numberofsteps.indexOf(selectedGameStepName);
+        selectedGameStepQR = gamesteps_qrString.get(index);
+
     }
     
 }
