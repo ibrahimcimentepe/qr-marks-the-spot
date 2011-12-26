7d8985d6b6fb6b9cd288884b2752531b5c7d1efc
alikerimerkan@gmail.com <alikerimerkan@gmail.com@47a1c95e-a7b4-0c84-69ca-ea3910dde828>
2011-12-26 13:10:09 +0000

    adminPage.java is created

---
+++ qrmarksspot/src/java/qrmarksspot/adminPage.java
@@ -0,0 +1,191 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package qrmarksspot;
+
+import com.sun.rave.faces.data.DefaultTableDataModel;
+import com.sun.rave.web.ui.appbase.AbstractPageBean;
+import javax.faces.FacesException;
+
+import Classes.Game;
+import Classes.GameBean;
+import java.util.ArrayList;
+import java.util.List;
+import java.sql.SQLException;
+/**
+ * <p>Page bean that corresponds to a similarly named JSP page.  This
+ * class contains component definitions (and initialization code) for
+ * all components that you have defined on this page, as well as
+ * lifecycle methods and event handlers where you may add behavior
+ * to respond to incoming events.</p>
+ *
+ * @version adminPage.java
+ * @version Created on Dec 25, 2011, 9:16:12 PM
+ * @author alke
+ */
+
+public class adminPage extends AbstractPageBean {
+    // <editor-fold defaultstate="collapsed" desc="Managed Component Definition">
+
+    /**
+     * <p>Automatically managed component initialization.  <strong>WARNING:</strong>
+     * This method is automatically generated, so any user-specified code inserted
+     * here is subject to being replaced.</p>
+     */
+    private void _init() throws Exception {
+    }
+    private DefaultTableDataModel dataTable1Model = new DefaultTableDataModel();
+
+    public DefaultTableDataModel getDataTable1Model() {
+        return dataTable1Model;
+    }
+
+    public void setDataTable1Model(DefaultTableDataModel dtdm) {
+        this.dataTable1Model = dtdm;
+    }
+
+    // </editor-fold>
+
+    //public List<Game>  mygames= new ArrayList<Game>();
+    GameBean bean = new GameBean();
+
+    public List<Game>  mygames = bean.getGameList();
+
+    public GameBean getBean() {
+        return bean;
+    }
+
+    public void setBean(GameBean bean) {
+        this.bean = bean;
+    }
+
+    public List<Game> getMygames() {
+        return mygames;
+    }
+
+    public void setMygames(List<Game> mygames) {
+        this.mygames = mygames;
+    }
+    private DefaultTableDataModel dataTable1Model1 = new DefaultTableDataModel();
+
+    public DefaultTableDataModel getDataTable1Model1() {
+        return dataTable1Model1;
+    }
+
+    public void setDataTable1Model1(DefaultTableDataModel dtdm) {
+        this.dataTable1Model1 = dtdm;
+    }
+   // public List<Game>  mygame = GameBean.getGameList();
+
+
+    /**
+     * <p>Construct a new Page bean instance.</p>
+     */
+    public adminPage() throws SQLException{
+    //dataTable1Model1.setWrappedData(this.mygames.toArray());
+         
+
+    }
+
+    /**
+     * <p>Callback method that is called whenever a page is navigated to,
+     * either directly via a URL, or indirectly via page navigation.
+     * Customize this method to acquire resources that will be needed
+     * for event handlers and lifecycle methods, whether or not this
+     * page is performing post back processing.</p>
+     * 
+     * <p>Note that, if the current request is a postback, the property
+     * values of the components do <strong>not</strong> represent any
+     * values submitted with this request.  Instead, they represent the
+     * property values that were saved for this view when it was rendered.</p>
+     */
+    @Override
+    public void init() {
+        // Perform initializations inherited from our superclass
+        super.init();
+        // Perform application initialization that must complete
+        // *before* managed components are initialized
+        // TODO - add your own initialiation code here
+        
+        // <editor-fold defaultstate="collapsed" desc="Managed Component Initialization">
+        // Initialize automatically managed components
+        // *Note* - this logic should NOT be modified
+        try {
+            _init();
+        } catch (Exception e) {
+            log("adminPage Initialization Failure", e);
+            throw e instanceof FacesException ? (FacesException) e: new FacesException(e);
+        }
+        
+        // </editor-fold>
+        // Perform application initialization that must complete
+        // *after* managed components are initialized
+        // TODO - add your own initialization code here
+    }
+
+    /**
+     * <p>Callback method that is called after the component tree has been
+     * restored, but before any event processing takes place.  This method
+     * will <strong>only</strong> be called on a postback request that
+     * is processing a form submit.  Customize this method to allocate
+     * resources that will be required in your event handlers.</p>
+     */
+    @Override
+    public void preprocess() {
+    }
+
+    /**
+     * <p>Callback method that is called just before rendering takes place.
+     * This method will <strong>only</strong> be called for the page that
+     * will actually be rendered (and not, for example, on a page that
+     * handled a postback and then navigated to a different page).  Customize
+     * this method to allocate resources that will be required for rendering
+     * this page.</p>
+     */
+    @Override
+    public void prerender() {
+    }
+
+    /**
+     * <p>Callback method that is called after rendering is completed for
+     * this request, if <code>init()</code> was called (regardless of whether
+     * or not this was the page that was actually rendered).  Customize this
+     * method to release resources acquired in the <code>init()</code>,
+     * <code>preprocess()</code>, or <code>prerender()</code> methods (or
+     * acquired during execution of an event handler).</p>
+     */
+    @Override
+    public void destroy() {
+    }
+
+    /**
+     * <p>Return a reference to the scoped data bean.</p>
+     *
+     * @return reference to the scoped data bean
+     */
+    protected ApplicationBean1 getApplicationBean1() {
+        return (ApplicationBean1) getBean("ApplicationBean1");
+    }
+
+    /**
+     * <p>Return a reference to the scoped data bean.</p>
+     *
+     * @return reference to the scoped data bean
+     */
+    protected RequestBean1 getRequestBean1() {
+        return (RequestBean1) getBean("RequestBean1");
+    }
+
+    /**
+     * <p>Return a reference to the scoped data bean.</p>
+     *
+     * @return reference to the scoped data bean
+     */
+    protected SessionBean1 getSessionBean1() {
+        return (SessionBean1) getBean("SessionBean1");
+    }
+    
+}
+
