0320d5fbebcd26238c95ef4df3a5b0fcae7a580f
alikerimerkan@gmail.com <alikerimerkan@gmail.com@47a1c95e-a7b4-0c84-69ca-ea3910dde828>
2011-12-26 13:08:30 +0000

    GameBean.java class added to be used by admin module

---
+++ qrmarksspot/src/java/Classes/GameBean.java
@@ -0,0 +1,87 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Classes;
+
+import java.io.Serializable;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.annotation.Resource;
+//import javax.faces.bean.ManagedBean;
+//import javax.faces.bean.SessionScoped;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+import org.apache.tomcat.util.modeler.ManagedBean;
+
+//import Classes.MySqlConnection;
+/**
+ *
+ * @author alke
+ */
+//@ManagedBean(name="GameBean1")
+//@SessionScoped
+public class GameBean {
+
+
+    public static List<Game>  games= new ArrayList<Game>();
+
+    public static List<Game> getGames() {
+        return games;
+    }
+
+    public static void setGames(List<Game> games) {
+        GameBean.games = games;
+    }
+
+    public List<Game> getGameList() throws SQLException{
+
+        try {
+           MySqlConnection con = new MySqlConnection();
+           ResultSet rs = con.getGamesList();
+
+           List<Game> list = new ArrayList<Game>();
+
+           while(rs.next())
+           {
+               Game game = new Game();
+
+               game.setBorders(rs.getString("Borders"));
+               game.setCreaterId(rs.getInt("CreaterId"));
+               game.setCreatorName(rs.getString("CreatorName"));
+               game.setCurrentStep(rs.getInt("CurrentStep"));
+               game.setDescription(rs.getString("Description"));
+               game.setGameId(rs.getInt("GameId"));
+               game.setGameName(rs.getString("GameName"));
+               game.setIsFinished(rs.getInt("IsFinished"));
+               game.setNumberOfPlayers(rs.getInt("NumberOfPlayers"));
+               game.setNumberOfSteps(rs.getInt("NumberOfSteps"));
+               game.setRating(rs.getInt("Rating"));
+               game.setStartLocation(rs.getString("StartLocation"));
+
+               list.add(game);
+           }
+
+           GameBean.games = list;
+            return list;
+        } catch(Exception e) {
+             System.out.println("Error");
+
+             return null;
+        }
+
+
+    }
+
+
+
+
+}
